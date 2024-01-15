6641fe3df5d1b1ad185333b50373193884a2cde4
Szymon Kulak <szymon.kulak@proton.me>
2024-01-15 12:47:37 +0100

    Completely Rewrote App Logic Made it *truly* foolproof Added freeCodeCamp testing suite TODO: - Remove fCC testing suite after passing tests - Write the Documentation

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,343 +1,317 @@
-import './Calculator.scss';
-import React, {useState} from 'react';
+import "./Calculator.scss";
+import React, { useState } from "react";
 
 function Calculator() {
+	const [formula, changeFormula] = useState("");
+	const [display, changeDisplay] = useState(0);
+	const [lastAction, changeAction] = useState("");
+	const [isDecimal, toggleDecimal] = useState(false);
+	const [isBlankNegative, toggleBlankNegative] = useState(false);
 
-    const[output, changeOutput] = useState(0);
-    const[history, changeHistory] = useState("");
-    const[result, changeResult] = useState(0);
-    const[currentCalc, changeCalc] = useState("");
+	const calculate = function () {
+		let tempResult = eval(formula);
+		changeFormula(formula + "=" + tempResult);
+		changeDisplay(tempResult);
+	};
 
-    const calculate = function() {
-        let returnVal;
-        switch (currentCalc) {
-            case "ADD":
-                returnVal = result + parseFloat(output)
-                changeResult(returnVal);
-                return(returnVal);
-            case "SUBTRACT":
-                returnVal = result - parseFloat(output)
-                changeResult(returnVal);
-                return(returnVal);
-            case "MULTIPLY":
-                returnVal = result * parseFloat(output)
-                changeResult(returnVal);
-                return(returnVal);
-            case "DIVIDE":
-                returnVal = result / parseFloat(output)
-                changeResult(returnVal);
-                return(returnVal);
-            default:
-                break;
-        }
-    }
+	const handleClick = function (e) {
+		let val = e.target.value;
 
-    const handleClick = function(e) {
-        let val = e.target.value;
-        let tempResult = 0;
+		switch (val) {
+			case "1":
+			case "2":
+			case "3":
+			case "4":
+			case "5":
+			case "6":
+			case "7":
+			case "8":
+			case "9":
+				if (lastAction === "operator" || lastAction === "minus") {
+					changeDisplay(val);
+					changeFormula(formula + val);
+				} else if (lastAction === "equals") {
+					changeFormula(val);
+					changeDisplay(val);
+				} else if (display !== 0) {
+					changeDisplay(display + val);
+					changeFormula(formula + val);
+				} else {
+					changeDisplay(val);
+					changeFormula(val);
+				}
+				changeAction("number");
+				toggleBlankNegative(false);
+				break;
+			case "0":
+				if (lastAction === "operator" || lastAction === "minus") {
+					changeDisplay(val);
+					changeFormula(formula + val);
+				} else if (lastAction === "equals") {
+					changeDisplay(val);
+					changeFormula("");
+				} else if (display !== 0) {
+					changeDisplay(display + val);
+					changeFormula(formula + val);
+				} else {
+					break;
+				}
+				break;
+			case "DECIMAL":
+				if (isDecimal) {
+					break;
+				}
 
-        switch(val) {
-            case "1":
-            case "2":
-            case "3":
-            case "4":
-            case "5":
-            case "6":
-            case "7":
-            case "8":
-            case "9":
-                if (!parseFloat(output)) {
-                    changeOutput(val);
-                    changeHistory(history+val);
-                } else if (output !== 0) {
-                    changeOutput(output+val);
-                    changeHistory(history+val);
-                } else {
-                    changeOutput(val);
-                    changeHistory(val);
-                }
-                break;
-            case "0":
-                if (!parseFloat(output)) {
-                    changeOutput(val);
-                    changeHistory(history+val);
-                } else if (output !== 0) {
-                    changeOutput(output+val);
-                    changeHistory(history+val);
-                } else {
-                    break;
-                }
-                break;
-            case "DECIMAL": 
-                if (!parseFloat(output)) {
-                    changeOutput("0.");
-                    changeHistory(history+"0.")
-                } else {
-                    changeOutput(output+".");
-                    changeHistory(history+".")
-                }
-                break;
-            case "ADD":
-                if (!history) {
-                    break;
-                }
-                if (currentCalc === "ADD" && output === "+") {
-                    break;
-                }
-                if (!parseFloat(output) && currentCalc !== "ADD") {
-                    changeCalc("ADD");
-                    changeOutput("+");
-                    changeHistory(history.slice(0,(history.length-1)) + "+")
-                    break;
-                }
+				if (lastAction === "operator" || lastAction === "minus") {
+					changeDisplay(display + "0.");
+					changeFormula(formula + "0.");
+					toggleDecimal(true);
+					toggleBlankNegative(false);
+					break;
+				}
 
-                if (result === 0) {
-                    changeResult(parseFloat(output));
-                } else {
-                    calculate();
-                }
+				if (lastAction === "equals") {
+					changeDisplay("0.");
+					changeFormula("0.");
+					toggleDecimal(true);
+					break;
+				}
 
-                changeCalc("ADD");
-                changeHistory(history + "+")
-                changeOutput("+");
-                break;
-            case "SUBTRACT":
-                if (!history) {
-                    break;
-                }
-                if (currentCalc === "SUBTRACT" && output === "-") {
-                    break;
-                }
-                if (!parseFloat(output) && currentCalc !== "SUBTRACT") {
-                    changeCalc("SUBTRACT");
-                    changeOutput("-");
-                    changeHistory(history.slice(0,(history.length-1)) + "-")
-                    break;
-                }
+				if (display === 0) {
+					changeDisplay(display + ".");
+					changeFormula(formula + "0.");
+					toggleDecimal(true);
+					break;
+				}
 
-                if (result === 0) {
-                    changeResult(parseFloat(output));
-                } else {
-                    calculate();
-                }
+				changeDisplay(display + ".");
+				changeFormula(formula + ".");
+				toggleDecimal(true);
+				break;
+			case "ADD":
+				if (isBlankNegative) {
+					changeFormula(formula.slice(0, formula.length - 2) + "+");
+					changeDisplay("+");
+					changeAction("operator");
+					toggleBlankNegative(false);
+					break;
+				}
 
-                changeCalc("SUBTRACT");
-                changeHistory(history + "-")
-                changeOutput("-");
-                break;
-            case "MULTIPLY":
-                if (!history) {
-                    break;
-                }
-                if (currentCalc === "MULTIPLY" && output === "*") {
-                    break;
-                }
-                if (!parseFloat(output) && currentCalc !== "MULTIPLY") {
-                    changeCalc("MULTIPLY");
-                    changeOutput("*");
-                    changeHistory(history.slice(0,(history.length-1)) + "*")
-                    break;
-                }
+				if (lastAction === "equals") {
+					changeFormula(display + "+");
+					changeDisplay("+");
+					changeAction("operator");
+					break;
+				}
 
-                if (result === 0) {
-                    changeResult(parseFloat(output));
-                } else {
-                    calculate();
-                }
+				if (lastAction === "operator" || lastAction === "minus") {
+					changeFormula(formula.slice(0, formula.length - 1) + "+");
+					changeDisplay("+");
+					changeAction("operator");
+					break;
+				}
 
-                changeCalc("MULTIPLY");
-                changeHistory(history + "*")
-                changeOutput("*");
-                break;
-            case "DIVIDE":
-                if (!history) {
-                    break;
-                }
-                if (currentCalc === "DIVIDE" && output === "/") {
-                    break;
-                }
-                if (!parseFloat(output) && currentCalc !== "DIVIDE") {
-                    changeCalc("DIVIDE");
-                    changeOutput("/");
-                    changeHistory(history.slice(0,(history.length-1)) + "/")
-                    break;
-                }
+				changeFormula(formula + "+");
+				changeDisplay("+");
+				changeAction("operator");
+				toggleDecimal(false);
+				break;
+			case "SUBTRACT":
+				if (lastAction === "equals") {
+					changeFormula(display + "-");
+					changeDisplay("-");
+					changeAction("minus");
+					break;
+				}
 
-                if (result === 0) {
-                    changeResult(parseFloat(output));
-                } else {
-                    calculate();
-                }
+				if (lastAction === "operator") {
+					toggleBlankNegative(true);
+				}
 
-                changeCalc("DIVIDE");
-                changeHistory(history + "/")
-                changeOutput("/");
-                break;    
-            case "EQUALS":
-                if(!currentCalc && !history){
-                    break;
-                } 
-                if (!currentCalc) {
-                    tempResult = output;
-                } else {
-                    tempResult = calculate();
-                }
-                changeOutput(tempResult);
-                changeCalc("");
-                break;
-            case "CLEAR":
-                changeHistory("");
-                changeOutput(0);
-                changeResult(0);
-                console.clear();
-                break;
-            default:
-                break;
-        }
-    }
+				if (lastAction === "minus") {
+					break;
+				}
 
-    return (
-        <div className="Calculator">
-            <div className="screen-top"> {history} </div>
-            <div className="screen-main" id="display"> {output} </div>
-            <div className="calcKeysContainer">
-                <button 
-                className="calcKey calcKeyWide calcKeyAC" 
-                id="clear" 
-                value="CLEAR"
-                onClick={handleClick}
-                >
-                    AC
-                </button>
-                <button 
-                className="calcKey calcKeyMath" 
-                id="divide" 
-                value="DIVIDE"
-                onClick={handleClick}
-                >
-                    /
-                </button>
-                <button 
-                className="calcKey calcKeyMath" 
-                id="multiply" 
-                value="MULTIPLY"
-                onClick={handleClick}
-                >
-                    x
-                </button>
-                <button 
-                className="calcKey" 
-                id="seven" 
-                value="7"
-                onClick={handleClick}
-                >
-                    7
-                </button>
-                <button 
-                className="calcKey" 
-                id="eight" 
-                value="8"
-                onClick={handleClick}
-                >
-                    8
-                </button>
-                <button 
-                className="calcKey" 
-                id="nine" 
-                value="9"
-                onClick={handleClick}
-                >
-                    9
-                </button>
-                <button 
-                className="calcKey calcKeyMath" 
-                id="subtract" 
-                value="SUBTRACT"
-                onClick={handleClick}
-                >
-                    -
-                </button>
-                <button 
-                className="calcKey" 
-                id="four" 
-                value="4"
-                onClick={handleClick}
-                >
-                    4
-                </button>
-                <button 
-                className="calcKey" 
-                id="five" 
-                value="5"
-                onClick={handleClick}
-                >
-                    5
-                </button>
-                <button 
-                className="calcKey" 
-                id="six" 
-                value="6"
-                onClick={handleClick}
-                >
-                    6
-                </button>
-                <button 
-                className="calcKey calcKeyMath" 
-                id="add" 
-                value="ADD"
-                onClick={handleClick}
-                >
-                    +
-                </button>
-                <button 
-                className="calcKey" 
-                id="one" 
-                value="1"
-                onClick={handleClick}
-                >
-                    1
-                </button>
-                <button 
-                className="calcKey" 
-                id="two" 
-                value="2"
-                onClick={handleClick}
-                >
-                    2
-                </button>
-                <button 
-                className="calcKey" 
-                id="three" 
-                value="3"
-                onClick={handleClick}
-                >
-                    3
-                </button>
-                <button 
-                className="calcKey calcKeyTall calcKeyEquals" 
-                id="equals"  
-                value="EQUALS"
-                onClick={handleClick}
-                >
-                    =
-                </button>
-                <button className="calcKey calcKeyWide" 
-                id="zero" 
-                value="0"
-                onClick={handleClick}
-                >
-                    0
-                </button>
-                <button className="calcKey" 
-                id="decimal" 
-                value="DECIMAL"
-                onClick={handleClick}
-                >
-                    .
-                </button>
-            </div>
-        </div>
-    );
+				changeFormula(formula + "-");
+				changeDisplay("-");
+				changeAction("minus");
+				toggleDecimal(false);
+				break;
+			case "MULTIPLY":
+				if (isBlankNegative) {
+					changeFormula(formula.slice(0, formula.length - 2) + "*");
+					changeDisplay("*");
+					changeAction("operator");
+					toggleBlankNegative(false);
+					break;
+				}
+
+				if (lastAction === "equals") {
+					changeFormula(display + "*");
+					changeDisplay("*");
+					changeAction("operator");
+					break;
+				}
+
+				if (lastAction === "operator" || lastAction === "minus") {
+					changeFormula(formula.slice(0, formula.length - 1) + "*");
+					changeDisplay("*");
+					changeAction("operator");
+					break;
+				}
+
+				changeFormula(formula + "*");
+				changeDisplay("*");
+				changeAction("operator");
+				toggleDecimal(false);
+				break;
+			case "DIVIDE":
+				if (isBlankNegative) {
+					changeFormula(formula.slice(0, formula.length - 2) + "/");
+					changeDisplay("/");
+					changeAction("operator");
+					toggleBlankNegative(false);
+					break;
+				}
+
+				if (lastAction === "equals") {
+					changeFormula(display + "/");
+					changeDisplay("/");
+					changeAction("operator");
+					break;
+				}
+
+				if (lastAction === "operator" || lastAction === "minus") {
+					changeFormula(formula.slice(0, formula.length - 1) + "/");
+					changeDisplay("/");
+					changeAction("operator");
+					break;
+				}
+
+				changeFormula(formula + "/");
+				changeDisplay("/");
+				changeAction("operator");
+				toggleDecimal(false);
+				break;
+			case "EQUALS":
+				if (lastAction === "equals") {
+					break;
+				}
+				calculate();
+				toggleDecimal(false);
+				changeAction("equals");
+				break;
+			case "CLEAR":
+				changeFormula("");
+				changeDisplay(0);
+				toggleDecimal(false);
+				break;
+			default:
+				break;
+		}
+	};
+
+	return (
+		<div className="Calculator">
+			<div className="screen-top"> {formula} </div>
+			<div className="screen-main" id="display">
+				{display}
+			</div>
+			<div className="calcKeysContainer">
+				<button
+					className="calcKey calcKeyWide calcKeyAC"
+					id="clear"
+					value="CLEAR"
+					onClick={handleClick}
+				>
+					AC
+				</button>
+				<button
+					className="calcKey calcKeyMath"
+					id="divide"
+					value="DIVIDE"
+					onClick={handleClick}
+				>
+					/
+				</button>
+				<button
+					className="calcKey calcKeyMath"
+					id="multiply"
+					value="MULTIPLY"
+					onClick={handleClick}
+				>
+					x
+				</button>
+				<button className="calcKey" id="seven" value="7" onClick={handleClick}>
+					7
+				</button>
+				<button className="calcKey" id="eight" value="8" onClick={handleClick}>
+					8
+				</button>
+				<button className="calcKey" id="nine" value="9" onClick={handleClick}>
+					9
+				</button>
+				<button
+					className="calcKey calcKeyMath"
+					id="subtract"
+					value="SUBTRACT"
+					onClick={handleClick}
+				>
+					-
+				</button>
+				<button className="calcKey" id="four" value="4" onClick={handleClick}>
+					4
+				</button>
+				<button className="calcKey" id="five" value="5" onClick={handleClick}>
+					5
+				</button>
+				<button className="calcKey" id="six" value="6" onClick={handleClick}>
+					6
+				</button>
+				<button
+					className="calcKey calcKeyMath"
+					id="add"
+					value="ADD"
+					onClick={handleClick}
+				>
+					+
+				</button>
+				<button className="calcKey" id="one" value="1" onClick={handleClick}>
+					1
+				</button>
+				<button className="calcKey" id="two" value="2" onClick={handleClick}>
+					2
+				</button>
+				<button className="calcKey" id="three" value="3" onClick={handleClick}>
+					3
+				</button>
+				<button
+					className="calcKey calcKeyTall calcKeyEquals"
+					id="equals"
+					value="EQUALS"
+					onClick={handleClick}
+				>
+					=
+				</button>
+				<button
+					className="calcKey calcKeyWide"
+					id="zero"
+					value="0"
+					onClick={handleClick}
+				>
+					0
+				</button>
+				<button
+					className="calcKey"
+					id="decimal"
+					value="DECIMAL"
+					onClick={handleClick}
+				>
+					.
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default Calculator;
